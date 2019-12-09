40f5427a5a5360098e75fd0b5a2bdc42edd395c6
Marko Ruman <marko.ruman@gmail.com>
2019-12-09 11:40:26 +0200

    second commit

--- README.md
+++ README.md
@@ -4,32 +4,3 @@ Implementation of a novel Mixture Ratio probabilistic model.
 
 Mathematical details can be found in the publications folder
 
-# Installation
-
-```
-pip install mixture-ratio
-```
-
-
-# Usage
-
-
-```
-from mixture-ratio import MixtureRatio
-
-#setting domain of all variables in (y, X)
-variables_domain = [10, 5, 7, 3, 2]
-
-#setting the mixture's structure
-variables_connection = [[0, 1, 2], [0, 3], [0, 4]]
-
-#initializing the MixtureRatio object
-mixture = MixtureRatio(variables_domain, variables_connection)
-
-#fitting the data
-mixture.fit(X, y)
-
-...
-# predicting probability of all classes for the given X
-mixture.predict_proba(X)
-```
--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 
-setup(name='mixture-ratio',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='MixtureRatio',
       version='0.0.00',
       url='https://github.com/marko-ruman/mixture-ratio',
       license='MIT',
       author='Marko Ruman',
       author_email='marko.ruman@gmail.com',
-      description='Implementation of novel Mixture Ratio probabilistic model',
+      description='Implementation of Mixture Ratio probabilistic model',
       packages=find_packages(),
-      long_description=open('README.md').read(),
+      # long_description=long_description,
       zip_safe=False)
